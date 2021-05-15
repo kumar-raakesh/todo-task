85bae624f4401b586b03362b981b8b46e67c540c
rakesh_kumar20 <rakesh4ever20@gmail.com>
2021-05-15 11:59:29 +0530

    react upgrade done

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,38 +1,36 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-class AboutUs extends Component {
-  componentDidMount = () => {
-    this.props.actions.testAction();
-  };
-
-  render() {
-    console.log("aaa: ", this.props);
-    return (
-      <div
+function AboutUs(props) {
+  useEffect(() => {
+    console.log("Hooks working", props);
+  }, []);
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button
+        style={{ padding: 8 }}
         onClick={() => {
-          this.props.history.push("/home");
+          props.history.push("/aboutUs");
         }}
       >
-        This is AboutUs
-        <div
-          style={{
-            height: 40,
-            background: "#f1f1f1",
-          }}
-        >
-          Test: {this.props.apps.test}
-        </div>
-        <div
-          style={{
-            height: 40,
-            background: "#f9f9f9",
-          }}
-        >
-          MSG: {this.props.apps.msg}
-        </div>
-      </div>
-    );
-  }
+        ABOUTUS
+      </button>
+      <button
+        onClick={() => {
+          props.history.push("/home");
+        }}
+        style={{ padding: 8 }}
+      >
+        HOME
+      </button>
+    </div>
+  );
 }
 
 export default AboutUs;
